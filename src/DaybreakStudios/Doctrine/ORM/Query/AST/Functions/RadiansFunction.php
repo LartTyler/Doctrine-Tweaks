6cbfc3136ffbbf8d1d249f4fee7f401888a9825b
Tyler Lartonoix <larttyler@live.com>
2015-05-22 11:10:26 -0400

    Added several new functions

---
+++ src/DaybreakStudios/Doctrine/ORM/Query/AST/Functions/RadiansFunction.php
@@ -0,0 +1,25 @@
+<?php
+	namespace DaybreakStudios\Doctrine\ORM\Query\AST\Functions;
+
+	use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+	use Doctrine\ORM\Query\Lexer;
+	use Doctrine\ORM\Query\Parser;
+	use Doctrine\ORM\Query\SqlWalker;
+
+	class RadiansFunction extends FunctionNode {
+		private $arg;
+
+		public function getSql(SqlWalker $walker) {
+			return sprintf('RADIANS(%f)', $this->arg->dispatch($walker));
+		}
+
+		public function parse(Parser $parser) {
+			$parser->match(Lexer::T_IDENTIFIER);
+			$parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+			$this->arg = $parser->ArithmeticPrimary();
+
+			$parser->match(Lexer::T_CLOSE_PARENTHESIS);
+		}
+	}
+?>
